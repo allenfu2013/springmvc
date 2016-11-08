8d2db2a1fd35bda7e6d9df476bc71fa6e88650f4
yong.fu <hope_fu@163.com>
2016-11-08 22:45:12 +0800

    add spring-test-dbunit

--- src/test/java/org/allen/springmvc/SpringBaseTest.java
+++ src/test/java/org/allen/springmvc/SpringBaseTest.java
@@ -1,14 +1,19 @@
 package org.allen.springmvc;
 
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring-test.xml")
-@TransactionConfiguration
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, TransactionalTestExecutionListener.class, TransactionDbUnitTestExecutionListener.class})
+@TransactionConfiguration(defaultRollback = true)
 public class SpringBaseTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 }
--- src/test/java/org/allen/springmvc/dao/UserDaoTest.java
+++ src/test/java/org/allen/springmvc/dao/UserDaoTest.java
@@ -1,5 +1,6 @@
 package org.allen.springmvc.dao;
 
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.allen.springmvc.SpringBaseTest;
 import org.allen.springmvc.entity.User;
 import org.junit.Test;
@@ -16,13 +17,16 @@ public class UserDaoTest extends SpringBaseTest {
     public void testInsert() {
         User user = new User();
         user.setName("test");
-        user.setSex("Male");
         long id = userDao.insert(user);
         assertTrue(id > 0);
     }
 
     @Test
+    @DatabaseSetup({"classpath:/dbunit/UserDaoTest.xml"})
     public void testGetById() {
-
+        User user = userDao.getById(100L);
+        assertNotNull(user);
+        assertEquals(100, user.getId().longValue());
+        assertEquals("name_100", user.getName());
     }
 }
